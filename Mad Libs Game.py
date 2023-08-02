9e65ffe4e4b135ab3ef91ab035d8842b50f19791
Sultanlodi <114211874+Sultanlodi@users.noreply.github.com>
2023-08-02 17:41:42 -0400

    Funny game of mad libs

---
+++ Mad Libs Game.py
@@ -0,0 +1,16 @@
+adjective = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+type_of_bird = input("Enter a type of bird: ")
+verbP = input("Enter a verb (past tense): ")
+verb = input("Enter a verb: ")
+relative_name = input("Enter a relative's name: ").capitalize()
+noun = input("Enter a noun: ")
+liquid = input("Enter a liquid: ")
+verbing = input("Enter a verb ending in ing: ")
+part_of_body = input("Enter a part of the body (plural): ")
+plural_noun = input("Enter a plural noun: ")
+
+
+print("It was a " + adjective + ",cold November Day. I woke up to the " + adjective2 + " smell of " + type_of_bird + " roasting in the downstairs. I " + verbP + " down the stairs to see if I could help " + verb)
+print('My mom said \"See if ' + relative_name + ' needs a fresh ' + noun + '.\" So I carried a tray of glasses full of ' + liquid+ ' into the ' + verbing + ' room. When I got there I couldn\'t believe my ' + part_of_body + '! There were ' + plural_noun + ' ' + verbing + ' on the ' + noun + '!')
+
